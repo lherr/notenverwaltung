06938356cf3f550994165b0cd875be22142b7a6b
Lutz Herrmann <lutz.herrqgmail.com>
2021-02-14 16:35:33 +0100

    Added models Course and Student

--- nv/models.py
+++ nv/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Student(models.Model):
+    tutor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    class_level = models.IntegerField()
+    
+class Course(models.Model):
+    teacher = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    short_name = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+    subject = models.CharField(max_length=200)
+    class_level = models.IntegerField()
+    

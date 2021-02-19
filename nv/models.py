a35ec6d4ef0582ab6c4633dd0413c9b1dd81bbf2
Lutz Herrmann <lutz.herrqgmail.com>
2021-02-19 17:12:53 +0100

    Added models TakePart

--- nv/models.py
+++ nv/models.py
@@ -16,3 +16,15 @@ class Course(models.Model):
     subject = models.CharField(max_length=200)
     class_level = models.IntegerField()
     
+class TakePart(models.Model):
+    studentId = models.ForeignKey('Student', on_delete=models.CASCADE)
+    courseId = models.ForeignKey('Course', on_delete=models.CASCADE)
+    k1 = models.IntegerField()
+    k2 = models.IntegerField()
+    n1 = models.IntegerField()
+    n2 = models.IntegerField()
+    n3 = models.IntegerField()
+    n4 = models.IntegerField()
+    n5 = models.IntegerField()
+    n6 = models.IntegerField()
+    e = models.IntegerField()
